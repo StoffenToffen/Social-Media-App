2c32c8688a0feba8390c8f0a97781754770861f3
Steffen Tveit <steffentveit@outlook.com>
2026-02-14 18:01:59 +0100

    Added Firebase user handling

--- components/SignUpPrompt.tsx
+++ components/SignUpPrompt.tsx
@@ -1,21 +1,29 @@
+"use client";
+
+import { useSelector } from "react-redux";
+import type { RootState } from "@/redux/store";
 import LogInModal from "./modals/LogInModal";
 import SignUpModal from "./modals/SignUpModal";
 
 const SignUpPrompt = () => {
+  const name = useSelector((state: RootState) => state.user.name);
+
   return (
-    <div className="fixed w-full h-20 bg-[#f4af01] bottom-0 flex justify-center items-center gap-5 lg:justify-between lg:px-20 xl:px-40 2xl:px-80">
-      <div className="hidden flex-col text-white md:flex">
-        <span className="text-xl font-bold">Don't miss out on the buzz</span>
+    !name && (
+      <div className="fixed w-full h-20 bg-[#f4af01] bottom-0 flex justify-center items-center gap-5 lg:justify-between lg:px-20 xl:px-40 2xl:px-80">
+        <div className="hidden flex-col text-white md:flex">
+          <span className="text-xl font-bold">Don't miss out on the buzz</span>
 
-        <span>People on Busy Bee are always the first to know.</span>
+          <span>People on Busy Bee are always the first to know.</span>
+        </div>
+
+        <div className="flex gap-2 w-full p-3 md:w-fit">
+          <LogInModal />
+
+          <SignUpModal />
+        </div>
       </div>
-
-      <div className="flex gap-2 w-full p-3 md:w-fit">
-        <LogInModal />
-
-        <SignUpModal />
-      </div>
-    </div>
+    )
   );
 };
 
--- components/modals/SignUpModal.tsx
+++ components/modals/SignUpModal.tsx
@@ -2,12 +2,23 @@
 
 import { EyeIcon, EyeSlashIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import { Modal } from "@mui/material";
-import { useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { auth } from "@/firebase";
 import { closeSignUpModal, openSignUpModal } from "@/redux/slices/modalSlice";
+import { signInUser } from "@/redux/slices/userSlice";
 import type { AppDispatch, RootState } from "@/redux/store";
 
 const SignUpModal = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
 
   const isOpen = useSelector(
@@ -15,11 +26,55 @@ const SignUpModal = () => {
   );
   const dispatch: AppDispatch = useDispatch();
 
+  const handleSignUp = async () => {
+    const userCredentials = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password,
+    );
+
+    console.log(userCredentials);
+
+    await updateProfile(userCredentials.user, {
+      displayName: name,
+    });
+
+    dispatch(
+      signInUser({
+        name: userCredentials.user.displayName,
+        username: userCredentials.user.email?.split("@")[0],
+        email: userCredentials.user.email,
+        uid: userCredentials.user.uid,
+      }),
+    );
+  };
+
+  const handleGuestLogIn = async () => {
+    await signInWithEmailAndPassword(auth, "guest123000@gmail.com", "12345678");
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (!currentUser) return;
+
+      dispatch(
+        signInUser({
+          name: currentUser.displayName,
+          username: currentUser.email?.split("@")[0],
+          email: currentUser.email,
+          uid: currentUser.uid,
+        }),
+      );
+    });
+
+    return unsubscribe;
+  }, [dispatch]);
+
   return (
     <>
       <button
         type="button"
-        className="w-full h-12 md:w-22 md:h-10 text-md md:text-sm font-bold bg-white rounded-full"
+        className="w-full h-12 md:w-22 md:h-10 text-md md:text-sm font-bold bg-white rounded-full cursor-pointer"
         onClick={() => dispatch(openSignUpModal())}
       >
         Sign Up
@@ -47,12 +102,16 @@ const SignUpModal = () => {
                 type="text"
                 className="w-full h-13.5 border border-gray-200 outline-none pl-3 rounded focus:border-[#f4af01] transition"
                 placeholder="Name"
+                onChange={(e) => setName(e.target.value)}
+                value={name}
               />
 
               <input
                 type="email"
                 className="w-full h-13.5 border border-gray-200 outline-none pl-3 rounded focus:border-[#f4af01] transition"
                 placeholder="Email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
               />
 
               <div className=" flex items-center w-full h-13.5 border border-gray-200 rounded focus-within:border-[#f4af01] transition pr-3">
@@ -60,6 +119,8 @@ const SignUpModal = () => {
                   type={showPassword ? "text" : "password"}
                   placeholder="Password"
                   className="w-full h-full outline-none pl-3"
+                  onChange={(e) => setPassword(e.target.value)}
+                  value={password}
                 />
 
                 <button
@@ -74,7 +135,8 @@ const SignUpModal = () => {
 
             <button
               type="button"
-              className="bg-[#f4af01] text-white h-12 rounded-full shadow-md mb-5 w-full"
+              className="bg-[#f4af01] text-white h-12 rounded-full shadow-md mb-5 w-full cursor-pointer"
+              onClick={handleSignUp}
             >
               Sign Up
             </button>
@@ -83,7 +145,8 @@ const SignUpModal = () => {
 
             <button
               type="button"
-              className="bg-[#f4af01] text-white h-12 rounded-full shadow-md mb-5 w-full"
+              className="bg-[#f4af01] text-white h-12 rounded-full shadow-md mb-5 w-full cursor-pointer"
+              onClick={handleGuestLogIn}
             >
               Log In as Guest
             </button>
