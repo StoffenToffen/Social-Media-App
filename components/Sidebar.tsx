8c11869a7d008deb3d90b2273896a75a377aa910
Steffen Tveit <steffentveit@outlook.com>
2026-02-18 00:36:40 +0100

    Fixed styling, and made it only possible to post if logged in

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -13,7 +13,7 @@ import SidebarUserInfo from "./SidebarUserInfo";
 const Sidebar = () => {
   return (
     <nav className="hidden sm:flex flex-col sticky top-0 p-3 h-screen xl:ml-20 xl:mr-10">
-      <div className="relative h-full flex flex-col items-center">
+      <div className="relative h-full flex flex-col">
         <div className="py-3">
           <Image
             src={"/assets/busybee-logo2.png"}
